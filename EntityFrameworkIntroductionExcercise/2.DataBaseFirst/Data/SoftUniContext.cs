91a9895486d4af3a3fbd1d8ffcfbe73e31a79ba7
monikaez <86738350+monikaez@users.noreply.github.com>
2023-12-24 16:52:57 +0200

    02

---
+++ EntityFrameworkIntroductionExcercise/2.DataBaseFirst/Data/SoftUniContext.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using SoftUni.Models;
+
+namespace SoftUni.Data
+
+{
+    public partial class SoftUniContext : DbContext
+    {
+        public SoftUniContext()
+        {
+        }
+
+        public SoftUniContext(DbContextOptions<SoftUniContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Address> Addresses { get; set; } = null!;
+        public virtual DbSet<Department> Departments { get; set; } = null!;
+        public virtual DbSet<Employee> Employees { get; set; } = null!;
+        public virtual DbSet<Project> Projects { get; set; } = null!;
+        public virtual DbSet<Town> Towns { get; set; } = null!;
+//add new maping table
+        public virtual DbSet<EmployeeProject> EmployeesProjects { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=.;Database =SoftUni;Integrated Security=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Address>(entity =>
+            {
+                entity.HasOne(d => d.Town)
+                    .WithMany(p => p.Addresses)
+                    .HasForeignKey(d => d.TownId)
+                    .HasConstraintName("FK_Addresses_Towns");
+            });
+
+            modelBuilder.Entity<Department>(entity =>
+            {
+                entity.HasOne(d => d.Manager)
+                    .WithMany(p => p.Departments)
+                    .HasForeignKey(d => d.ManagerId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Departments_Employees");
+            });
+
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.HasOne(d => d.Address)
+                    .WithMany(p => p.Employees)
+                    .HasForeignKey(d => d.AddressId)
+                    .HasConstraintName("FK_Employees_Addresses");
+
+                entity.HasOne(d => d.Department)
+                    .WithMany(p => p.Employees)
+                    .HasForeignKey(d => d.DepartmentId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Employees_Departments");
+
+                entity.HasOne(d => d.Manager)
+                    .WithMany(p => p.InverseManager)
+                    .HasForeignKey(d => d.ManagerId)
+                    .HasConstraintName("FK_Employees_Employees");
+
+               
+            });
+
+            modelBuilder.Entity<EmployeeProject>(entity =>
+            {
+                entity.HasKey(pk => new { pk.EmployeeId, pk.ProjectId });
+
+                entity
+                    .HasOne(e => e.Employee)
+                    .WithMany(e => e.EmployeesProjects)
+                    .HasForeignKey(ep => ep.EmployeeId);
+
+                entity
+                    .HasOne(e => e.Project)
+                    .WithMany(p => p.EmployeesProjects)
+                    .HasForeignKey(ep => ep.ProjectId);
+
+            });
+            //modelBuilder.Entity<EmployeeProject>(entity =>
+            //{
+            //    entity.HasKey(pk => new
+            //    {//PK V MAPING TABLE
+            //        pk.EmployeeId,
+            //        pk.ProjectId
+            //    });
+            //    //change Employee.cs
+            //    entity.HasOne(ep => ep.Employee)
+            //    .WithMany(e => e.EmployeesProjects)
+            //    .HasForeignKey(ep => ep.EmployeeId);
+            //    //change Project.cs
+            //    entity.HasOne(ep => ep.Project)
+            //    .WithMany(p => p.EmployeesProjects)
+            //    .HasForeignKey(ep => ep.ProjectId);
+
+            //});
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ EntityFrameworkIntroductionExcercise/2.DataBaseFirst/StartUp.cs
@@ -0,0 +1,153 @@
+﻿
+using SoftUni.Data;
+using SoftUni.Models;
+using System.Globalization;
+using System.Text;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        SoftUniContext context = new();
+        string output = string.Empty;
+
+        //3. output = GetEmployeesFullInformation(context);
+        //4. output = GetEmployeesWithSalaryOver50000(context);
+        //5. output = GetEmployeesFromResearchAndDevelopment(context);
+        //6.output = AddNewAddressToEmployee(context);
+        output = GetEmployeesInPeriod(context);
+        Console.WriteLine(output);
+
+    }
+    //3.public static string GetEmployeesFullInformation(SoftUniContext context) and public StartUp class. 
+    public static string GetEmployeesFullInformation(SoftUniContext context)
+    {
+        var employees = context.Employees
+            .Select(e => new
+            {
+                e.FirstName,
+                e.LastName,
+                e.MiddleName,
+                e.JobTitle,
+                e.Salary
+            }).ToList();
+
+        string result = string.Join(Environment.NewLine, employees
+            .Select(e => $"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:f2}"));
+
+        return result;
+    }
+
+
+
+    //4.You will need method public static string GetEmployeesWithSalaryOver50000(SoftUniContext context) and public StartUp class. 
+    public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+    {
+        var employees = context.Employees
+            .Select(e => new
+            {
+                e.FirstName,
+                e.Salary
+            }).Where(e => e.Salary > 50000)
+            .OrderBy(e => e.FirstName)
+            .ToList();
+
+        return string.Join(Environment.NewLine, employees.Select(e => $"{e.FirstName} - {e.Salary:f2}"));//"{firstName} – {salary}"
+    }
+
+    //5.	Employees from Research and Development
+    // NOTE: You will need method public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context) and public StartUp class. 
+
+    public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+    {
+        var rndEmployees = context.Employees
+            .Where(e => e.Department.Name == "Research and Development")
+            .Select(e => new
+            {
+                e.FirstName,
+                e.LastName,
+                e.Department.Name,
+                e.Salary
+            }).OrderBy(e => e.Salary)
+              .ThenByDescending(e => e.FirstName);
+        return string.Join(Environment.NewLine, rndEmployees
+            .Select(e => $"{e.FirstName} {e.LastName} from {e.Name} - ${e.Salary:f2}"));
+    }
+
+    //6.Adding a New Address and Updating Employee
+    //NOTE: You will need method public static string AddNewAddressToEmployee(SoftUniContext context) and public StartUp class. 
+    public static string AddNewAddressToEmployee(SoftUniContext context)
+    {
+        //Setting the address
+        Address address = new Address();
+        address.AddressText = "Vitoshka 15";
+        address.TownId = 4;
+
+        context.Addresses.Add(address);
+        context.SaveChanges();
+
+        // search Employee
+        var searchedEmployee = context.Employees
+            .Where(e => e.LastName == "Nakov")
+            .FirstOrDefault();
+
+        searchedEmployee.Address = address;
+        context.SaveChanges();
+
+        //Select employees
+        var employees = context.Employees
+            .Select(e => new { e.AddressId, e.Address })
+            .OrderByDescending(e => e.AddressId)
+            .Take(10);
+
+        //Output
+        StringBuilder sb = new StringBuilder();
+
+        foreach (var employee in employees)
+        {
+            sb.AppendLine($"{employee.Address.AddressText}");
+        }
+
+        return sb.ToString().TrimEnd();
+    }
+
+    //7.7.	Employees and Projects
+    // NOTE: You will need method public static string GetEmployeesInPeriod(SoftUniContext context) and public StartUp class. 
+    public static string GetEmployeesInPeriod(SoftUniContext context)
+    {
+        var EmployeeInfo = context.Employees
+            .Take(10)
+            .Select(e => new
+            {
+                e.FirstName,
+                e.LastName,
+                ManagerFirstName = e.Manager.FirstName,
+                ManagerLastName = e.Manager.LastName,
+                Projects = e.EmployeesProjects.Where(ep => ep.Project.StartDate.Year >= 2001 & ep.Project.StartDate.Year <= 2003)
+                    .Select(ep => new
+                    {
+                        ProjectName = ep.Project.Name,
+                        StartDate = ep.Project.StartDate.ToString("M/d/yyyy h:mm:ss tt", CultureInfo.InvariantCulture),
+                        EndDate = ep.Project.EndDate != null
+                            ? ep.Project.EndDate.Value.ToString("M/d/yyyy h:mm:ss tt", CultureInfo.InvariantCulture)
+                            : "not finished"
+                    })
+            })
+            .ToList();
+
+        StringBuilder sb = new StringBuilder();
+
+        foreach (var e in EmployeeInfo)
+        {
+            sb.AppendLine($"{e.FirstName} {e.LastName} - Manager: {e.ManagerFirstName} {e.ManagerLastName}");
+
+            if (e.Projects.Any())
+            {
+                sb.AppendLine(String.Join(Environment.NewLine, e.Projects
+                    .Select(p => $"--{p.ProjectName} - {p.StartDate} - {p.EndDate}")));
+            }
+        }
+
+        return sb.ToString().TrimEnd();
+    }
+}
